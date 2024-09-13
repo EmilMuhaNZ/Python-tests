6c4cbd71b96503ae44d54f9591ca119732fb05b5
EmilMuhaNZ <emilmuhanz@gmail.com>
2024-09-13 22:14:59 +1200

    get_command done. Figuring out to branch the options

--- To_do_list/todolist.py
+++ To_do_list/todolist.py
@@ -1,5 +1,6 @@
 
 
+
 to_do_list = []
 
 def introduction_message():
@@ -14,7 +15,23 @@ def print_options():
     print("3. Remove a task")
     print("4. Exit")
 
+def add_a_task(existing_list):
+    new_task = input("Please enter which task you would like to add: ")
+    existing_list += [new_task]
+    return existing_list
+
+def get_command():
+    command = int(input("Please select an option: "))
 
 
-introduction_message()
-print_options()
+def display_list(existing_list):
+    if len(existing_list) == 0:
+        print("The list is currently empty, no tasks to desplay!")
+    else:
+        for i in range (len(existing_list)):
+            print(f"{i + 1}. {existing_list[i]}")
+
+def exit_message():
+    print("Thank you for helping Emil test this code!")
+
+display_list()
